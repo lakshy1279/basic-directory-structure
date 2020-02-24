b0f6b2034a98f5a1295ba99c340646afe00739de
lakshy pachory <luckyron1279@gmail.com>
2020-02-24 12:32:44 +0530

    add another route and controller

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express=require('express');
 const router=express.Router();
 const homeController=require('../controller/home_controller');
 router.get('/',homeController.home);
+router.use('/users',require('./user'));
 console.log('router loaded');
 
 module.exports=router;
