34664af02da7c909207d92130977bd78efaf247a
lakshy pachory <luckyron1279@gmail.com>
2020-02-24 12:41:22 +0530

    add view engine and file

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express=require('express');
 const port=8000;
 const app=express();
 //use express router
-app.use('/',require('./routes/index'))
+app.use('/',require('./routes/index'));
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err)
 {
     if(err)
