295126e93d32b0d4b403d71b51a2f66a606de3c3
lakshy pachory <luckyron1279@gmail.com>
2020-02-28 22:37:16 +0530

    add posts and send data to the database

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router=express.Router();
 const homeController=require('../controller/home_controller');
 router.get('/',homeController.home);
 router.use('/users',require('./user'));
+router.use('/posts',require('./post'));
 console.log('router loaded');
 
 module.exports=router;
---
+++ routes/post.js
@@ -0,0 +1,6 @@
+const express=require('express');
+const router=express.Router();
+const postController=require('../controller/post_controller');
+router.post('/create',postController.create);
+
+module.exports=router;
